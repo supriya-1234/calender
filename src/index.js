809d8ef0ba7c9503749ecaa4a51e2e1a71b6afbe
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2021-01-13 13:51:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calendar from "./components/Calendar";
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+
+import reducer from "./reducers";
+
+const middleware = [];
+if (process.env.NODE_ENV !== "production") {
+  middleware.push(createLogger());
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Calendar />
+  </Provider>,
+  document.getElementById("root")
+);
